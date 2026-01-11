fb61a32e0daab77f9cb7522e08cf5834021db2ab
DanielSouza2005 <danisimoes741@gmail.com>
2026-01-10 21:34:20 -0300

    Add CORS configuration, DTO for series data, and update SerieController to return series list

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,13 +1,28 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.dto.DadosSerieDTO;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository serieRepository;
+
     @GetMapping("/series")
-    public String obterSeries() {
-        return "Hello World!";
+    public List<DadosSerieDTO> obterSeries() {
+        return serieRepository.findAll()
+                .stream()
+                .map(s -> new DadosSerieDTO(
+                        s.getId(), s.getTitulo(), s.getTotalTemporadas(),
+                        s.getAvaliacao(), s.getGenero(), s.getAtores(),
+                        s.getImagem(), s.getSinopse()))
+                .collect(Collectors.toList());
     }
 }
